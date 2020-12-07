bc52b400e11e983209f2b3835bf80aac140baf00
vamshirajmudrakolla <72905580+vamshirajmudrakolla@users.noreply.github.com>
2020-12-07 17:49:40 +0530

    dummy strings file.

---
+++ Python/Strings2.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+seq_string = 'peter piper picked a peck of pickled peppers.'
+print(list(reversed(seq_string)))
+
+
+# In[5]:
+
+
+a ="this is string"
+a.capitalize()
+
+
+# In[8]:
+
+
+a.upper()
+
+
+# In[11]:
+
+
+a.count("1")
+
+
+# In[12]:
+
+
+a.title()
+
+
+# In[17]:
+
+
+print('#'*30)
+
+
+# In[28]:
+
+
+list1=[1,1,3,2,5,7]
+print(list1.index(3))
+
+
+# In[46]:
+
+
+my_list=[1,2,2,1,2,5,7,8,9,4,54,64,84,78]
+print(my_list[-3])
+
+
+# In[48]:
+
+
+my_list[1:8]
+
+
+# In[49]:
+
+
+my_list[5:]
+
+
+# In[50]:
+
+
+my_list[:4]
+
+
+# In[8]:
+
+
+str = "Hello World"
+str.upper()
+str.capitalize()
+str.count("l")
+str.find("o")
+
+
+# In[9]:
+
+
+str = "Hello World"
+str.replace("Hello","Hii")
+
+
+# In[12]:
+
+
+inputStr ='Hello'
+print(inputStr[-1::-1])
+
+
+# In[14]:
+
+
+inputStr='Vamshi'
+print(inputStr[-1::-1])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
